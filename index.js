dc0239a01b26b57fc44b623827290f8e7e7a4af3
UnicodeMatin <96652638+UnicodeMatin@users.noreply.github.com>
2021-12-26 15:39:47 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,46 +2,22 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 client.on("ready", () => {
-  console.log("Bot is ready."); 
+  console.log("RGB Started."); 
   if(config.slowmode < 60000) console.log(`This slowmode is dangerous!!!`)
   let colors = [
-    "#faf5b3",
-    "#ffd8a1",
-    "#f9a98e",
-    "#f7aec1",
-    "#e7b4d3",
-    "#c5acd3",
-    "#b0a9d3",
-    "#acb8de",
-    "#b8def5",
-    "#c5e5d6",
-    "#c5e2b6",
-    "#d5e6af",
-    "#f9f18c",
-    "#fec679",
-    "#f68567",
-    "#f489a7",
-    "#df92be",
-    "#ac8bc0",
-    "#9087c0",
-    "#879bce",
-    "#96d1f3",
-    "#addac6",
-    "#abd596",
-    "#c1dd89",
-    "#f6ed60",
-    "#fdb64e",
-    "#f15b40",
-    "#f15f90",
-    "#d671ad",
-    "#9667ab",
-    "#756bb0",
-    "#6783c2",
-    "#70c3ed",
-    "#8ed0b5",
-    "#8ec975",
-    "#aed361"
-  ] 
+    "#f4eb20",
+    "#fba51a",
+    "#ed1e24",
+    "#ec197a",
+    "#d1499b",
+    "#8750a1",
+    "#5b52a3",
+    "#436fb6",
+    "#40b8ea",
+    "#72c7a6",
+    "#72bf55",
+    "#9bca3c"
+  ]
   setInterval(() => {
     let cor = colors[Math.floor(Math.random() * colors.length)];
     client.guilds.cache.get(config.guild).roles.cache.get(config.role).setColor(cor)
